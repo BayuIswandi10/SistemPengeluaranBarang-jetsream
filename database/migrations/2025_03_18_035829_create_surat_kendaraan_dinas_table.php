85552365ce1d9e06efb1858a1e7e403c30c94505
BayuIswandi10 <0320220003@polman.astra.ac.id>
2025-06-04 01:15:48 +0700

    new

--- database/migrations/2025_03_18_035829_create_surat_kendaraan_dinas_table.php
+++ database/migrations/2025_03_18_035829_create_surat_kendaraan_dinas_table.php
@@ -24,7 +24,6 @@ return new class extends Migration
             $table->dateTime('expired_date')->useCurrent();
             $table->string('expired_status', 15);
             $table->string('kilometer_awal', 35)->nullable();
-            $table->string('kilometer_akhir', 35)->nullable();
             $table->string('alasan_penolakan', 500)->nullable();
 
             $table->foreign('created_by')
